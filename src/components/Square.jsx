54bf07a94fe2594282277dbe12865a7ba9339230
Nikolay-S-Nikolov <nikolay.s.nikolov@gmail.com>
2025-11-04 10:26:27 +0200

    add value state to Square component

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,11 +1,16 @@
-export default function Square({ value }) {
+import { useState } from "react";
+
+export default function Square() {
+
+    const [value, setValue] = useState(null);
+
+    const onSquareClickHendler = () => {
+        setValue('X');
+    };
+    
     return (
-        <button className="tic-button" onClick={onSquareClick}>
+        <button className="tic-button" onClick={onSquareClickHendler}>
             {value}
         </button>
     );
-}
-
-function onSquareClick(e) {
-    console.log('clicked')
 }
